777090eb92f890267fa2f2c03b47fc13cdc40434
rpm3 <mancuso.b3@gmail.com>
2023-10-31 08:36:54 -0500

    Added first draft of schema, defined workflow in server.js

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+)
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dep_name VARCHAR (30) NOT NULL,
+)
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    dep_id INT,
+    FOREIGN KEY (dep_id)
+    REFERENCES department(id)
+)
---
+++ server.js
@@ -0,0 +1,11 @@
+const inquirer = require ('inquirer');
+const express = require ('express');
+const mysql = require('mysql2');
+
+
+
+// class recording 
+// end of day thursday, 26 oct @ 2:17 pm
+
+// process
+// schema, seeds, queries in workbench, then put it in js and work it
